d9efe873e6eaab6f519ac400c905fd26b0a98774
daddymac77 <cri22004@byui.edu>
2023-01-14 23:29:10 +0800

    lots of changes

---
+++ scripts/lesson-2.js
@@ -0,0 +1,9 @@
+
+let oLastModif = new Date(document.lastModified);
+// Step 4: place the value of the current year variable into the HTML file
+document.querySelector("#year").innerText = oLastModif;
+
+const newName = document.querySelector("#name");
+
+const squavaPic = "images/SQUAVA.png";
+document.querySelector("#squava-image").setAttribute
